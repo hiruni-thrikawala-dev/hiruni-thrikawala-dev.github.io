8dc0780ea38c1706d35d289700887958328236de
TBKaumadhi <kawmadhithrikawala@gmail.com>
2025-09-09 18:02:19 +0530

    Use HashRouter for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -16,5 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-   base: mode === 'production' ? '/portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/myportfolio/' : '/',
 }));
