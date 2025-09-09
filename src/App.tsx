a34d1f3cabd8367f8dfc9c38b80f956ffe70fe61
TBKaumadhi <kawmadhithrikawala@gmail.com>
2025-09-09 22:00:06 +0530

    Fix production build for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename={import.meta.env.PROD ? '/myportfolio' : ''}>
+      <HashRouter basename={import.meta.env.PROD ? '/portfolio' : ''}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
